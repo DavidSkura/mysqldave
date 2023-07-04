b12a8b2224743665885cf54a42a48a6aa42343eb
Dave <109882672+daveskura@users.noreply.github.com>
2023-07-04 12:10:11 -0400

    updates

--- Admin Query SQL.sql
+++ Admin Query SQL.sql
@@ -1,11 +1,9 @@
-# Show table locks
 ```
 -- show table locks
 show open tables 
 where In_Use > 0 ;
 ```
 
-# Find Running Queries
 ```
 -- Find Running Queries
 SELECT
@@ -19,19 +17,18 @@ FROM SYS.PROCESSLIST A
 WHERE db is not null and command is not null;
 ```
 
-# stop running query
 ```
 -- stop running query
 KILL QUERY PID
 ```
 
-# show users
 ```
 -- show users
 SELECT *
 FROM mysql.user;
 ```
-# show current logged in user
+
+
 ```
 -- show current logged in user
 SELECT current_user();
