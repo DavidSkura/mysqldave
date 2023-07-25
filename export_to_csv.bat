7b1e2ecbd8838de0803ace9c89021b29e8327343
David Skura <140424191+DavidSkura@users.noreply.github.com>
2023-07-25 12:25:24 -0400

    Update export_to_csv.bat

--- export_to_csv.bat
+++ export_to_csv.bat
@@ -4,5 +4,6 @@ REM  Dave Skura, 2023
 REM
 
 "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u dave -p -t  -T. ossn ossn_users --fields-terminated-by=','
+mysql -u dbadmin -p mysql -h localhost -e "SELECT concat(app_contact_name,',',app_contact2_name,',',branch_phone,',',buyer_phone,',',builder_sales_phone,',',app_city) as line FROM atable" >filename.csv
 REM "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -u dave ossn -h localhost -e "SELECT * INTO OUTFILE 'F:\\git\\mysql_basics\\ossn_users.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' FROM ossn_users;"
 REM "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -u dave ossn -h localhost -e "SELECT * FROM ossn_users" > ossn_users.tsv
